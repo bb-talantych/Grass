08126b2e1bef189906a95bd97688e60f9165ae34
Vladyslav Michurin <goduster.mail@gmail.com>
2026-02-17 04:01:05 -0500

    Instancing
    Somewhat working instancing

--- Grass/Assets/Billboard Grass/Grass Controller.cs
+++ Grass/Assets/Billboard Grass/Grass Controller.cs
@@ -1,46 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrassController : MonoBehaviour
 {
-    struct GrassData
-    {
-        public Vector3 position;
-    };
-
-    [Range(1f, 1000f)]
+    [Range(1, 1000)]
     public int grassFieldSize = 300;
+    public Mesh grassMesh;
+    public Material grassMaterial;
+    public ComputeShader grassComputeShader;
 
-    public Transform grassPrefab;
-    public ComputeShader grassPosShader;
+    private ComputeBuffer grassBuffer;
+    private ComputeBuffer argsBuffer;
+    private int totalInstances;
 
     void Start()
     {
-        int bufferSize = sizeof(float) * 3;
-        int arraySize = grassFieldSize * grassFieldSize;
+        totalInstances = grassFieldSize * grassFieldSize;
+        grassBuffer = new ComputeBuffer(totalInstances, sizeof(float) * 3);
+        Vector3[] dummy = new Vector3[totalInstances];
+        grassBuffer.SetData(dummy);
 
-        ComputeBuffer grassBuffer = new ComputeBuffer(arraySize, bufferSize);
+        int kernelIndex = grassComputeShader.FindKernel("GetGrassData");
+        grassComputeShader.SetBuffer(kernelIndex, "grassDataBuffer", grassBuffer);
+        grassComputeShader.SetInt("grassFieldSize", grassFieldSize);
 
-        GrassData[] grassDataArray = new GrassData[arraySize];
-        grassBuffer.SetData(grassDataArray);
+        int threadGroups = Mathf.CeilToInt(grassFieldSize / 8f);
+        grassComputeShader.Dispatch(kernelIndex, threadGroups, threadGroups, 1);
 
-        int kernelIndex = grassPosShader.FindKernel("GetGrassPos");
-        grassPosShader.SetBuffer(kernelIndex, "grassDataBuffer", grassBuffer);
-        grassPosShader.SetInt("grassFieldSize", grassFieldSize);
-        int threadGroups = Mathf.CeilToInt(grassFieldSize / 8.0f);
-        grassPosShader.Dispatch(kernelIndex, threadGroups, threadGroups, 1);
+        grassMaterial.enableInstancing = true;
+        grassMaterial.SetBuffer("positionBuffer", grassBuffer);
 
-        grassBuffer.GetData(grassDataArray);
-
-        for (int i = 0; i < arraySize; i++)
+        uint[] args = new uint[5]
         {
-            Vector3 grassPos = grassDataArray[i].position;
+            grassMesh.GetIndexCount(0),
+            (uint)totalInstances,
+            0, 0, 0
+        };
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        argsBuffer.SetData(args);
+    }
 
-            Debug.Log("Grass number " + i + " is at " + grassPos);
-        }
+    void Update()
+    {
+        Graphics.DrawMeshInstancedIndirect(
+            grassMesh,
+            0,
+            grassMaterial,
+            new Bounds(Vector3.zero, new Vector3(grassFieldSize, 10f, grassFieldSize)),
+            argsBuffer
+        );
+    }
 
-
-        grassBuffer.Dispose();
+    void OnDestroy()
+    {
+        grassBuffer?.Dispose();
+        argsBuffer?.Dispose();
     }
 }
---
+++ Grass/Assets/Billboard Grass/GrassInstancing.shader
@@ -0,0 +1,69 @@
+Shader "_BB/GrassInstancing"
+{
+    Properties
+    {
+        _MainTex("Main Texture", 2D) = "white" {}
+        _Color("Color", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags {"Queue"="Transparent" "RenderType"="Transparent"}
+        
+        Cull Off
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_instancing
+
+            #include "UnityCG.cginc"
+
+            StructuredBuffer<float3> positionBuffer;
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            float4 _Color;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert(appdata v, uint id : SV_InstanceID)
+            {
+                v2f o;
+
+                float3 offset = positionBuffer[id];
+                float4 worldPos = float4(v.vertex.xyz + offset, 1.0);
+                o.vertex = UnityObjectToClipPos(worldPos);
+
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                float4 mainTex = tex2D(_MainTex, i.uv);
+                return mainTex * _Color;
+            }
+            ENDCG
+        }
+    }
+}
+
