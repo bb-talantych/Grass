a69ee14c5b8ff0242a038be196b20a147f250054
Vladyslav Michurin <goduster.mail@gmail.com>
2026-02-17 03:11:10 -0500

    GrassData compute shader
    Compute shader works, now figuring how to make gpu instancing work

--- Grass/Assets/Billboard Grass/Grass Controller.cs
+++ Grass/Assets/Billboard Grass/Grass Controller.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class GrassController : MonoBehaviour
 {
-    [Range(1f, 1000f)]
-    public int grassSize = 300;
+    struct GrassData
+    {
+        public Vector3 position;
+    };
 
-    public GameObject grassPrefab;
+    [Range(1f, 1000f)]
+    public int grassFieldSize = 300;
+
+    public Transform grassPrefab;
+    public ComputeShader grassPosShader;
 
     void Start()
     {
-        for(int i = 0; i < grassSize; i++) 
+        int bufferSize = sizeof(float) * 3;
+        int arraySize = grassFieldSize * grassFieldSize;
+
+        ComputeBuffer grassBuffer = new ComputeBuffer(arraySize, bufferSize);
+
+        GrassData[] grassDataArray = new GrassData[arraySize];
+        grassBuffer.SetData(grassDataArray);
+
+        int kernelIndex = grassPosShader.FindKernel("GetGrassPos");
+        grassPosShader.SetBuffer(kernelIndex, "grassDataBuffer", grassBuffer);
+        grassPosShader.SetInt("grassFieldSize", grassFieldSize);
+        int threadGroups = Mathf.CeilToInt(grassFieldSize / 8.0f);
+        grassPosShader.Dispatch(kernelIndex, threadGroups, threadGroups, 1);
+
+        grassBuffer.GetData(grassDataArray);
+
+        for (int i = 0; i < arraySize; i++)
         {
-            GameObject grass = Instantiate(grassPrefab);
-            grass.transform.parent = transform;
+            Vector3 grassPos = grassDataArray[i].position;
+
+            Debug.Log("Grass number " + i + " is at " + grassPos);
         }
+
+
+        grassBuffer.Dispose();
     }
 }
--- Grass/Assets/Testing/ComputeShadertest.cs
+++ Grass/Assets/Testing/ComputeShadertest.cs
@@ -1,26 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public struct Cube
+{
+    public Vector3 position;
+    public Color color;
+}
 
 public class ComputeShadertest : MonoBehaviour
 {
     public ComputeShader computeShader;
     public RenderTexture renderTexture;
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    public Mesh mesh;
+    public Material material;
+    public int count = 50;
+    [Range(1, 50000)]
+    public int repetitions = 1;
+
+    private List<GameObject> objects;
+    private Cube[] data;
+
+    public void CreateCubes()
     {
-        if (renderTexture == null)
+        objects = new List<GameObject>();
+
+        data = new Cube[count * count];
+
+        for(int i = 0; i < count; i++)
         {
-            renderTexture = new RenderTexture(256, 256, 24);
-            renderTexture.enableRandomWrite = true;
-            renderTexture.Create();
+            for(int j = 0; j < count; j++)
+            {
+                CreateCube(i, j);
+            }
+        }
+    }
+    void CreateCube(int x, int y)
+    {
+        GameObject cube = new GameObject("Cube" + x * count + y, typeof(MeshFilter), typeof(MeshRenderer));
+        cube.GetComponent<MeshFilter>().mesh = mesh;
+        cube.GetComponent<MeshRenderer>().material = new Material(material);
+        float xPos = x - count * 0.5f;
+        float yPos = y - count * 0.5f;
+        cube.transform.position = new Vector3(xPos, yPos, Random.Range(-0.1f, 0.1f));
+
+        Color color = Random.ColorHSV();
+        cube.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
+
+        objects.Add(cube);
+
+        Cube cubeData = new Cube();
+        cubeData.position = cube.transform.position;
+        cubeData.color = color;
+
+        data[x * count + y] = cubeData;
+
+    }
+
+    private void OnRandomizeGPU()
+    {
+        int colorSize = sizeof(float) * 4;
+        int vecotr3Size = sizeof(int) * 3;
+        int totalSize = colorSize + vecotr3Size;    
+
+        ComputeBuffer cubesBuffer = new ComputeBuffer(data.Length, totalSize);
+        cubesBuffer.SetData(data);
+
+        int kernel = computeShader.FindKernel("CSMain");
+        computeShader.SetBuffer(kernel, "cubes", cubesBuffer);
+        computeShader.SetFloat("resolution", data.Length);
+        computeShader.SetInt("repetitions", repetitions);
+        computeShader.Dispatch(kernel, data.Length / 10, 1, 1);
+
+        cubesBuffer.GetData(data);
+
+        for (int i = 0; i < objects.Count; i++) 
+        {
+            GameObject obj = objects[i];
+            Cube cube = data[i];
+            obj.transform.position = cube.position;
+            obj.GetComponent<MeshRenderer>().material.SetColor("_Color", cube.color);
         }
 
-        int kernelIndex = 0;
-        computeShader.SetTexture(kernelIndex, "Result", renderTexture);
-        computeShader.SetFloat("Resolution", renderTexture.width);
-        computeShader.Dispatch(kernelIndex, renderTexture.width / 8, renderTexture.height / 8, 1);
+        cubesBuffer.Dispose();
+    }
 
-        Graphics.Blit(renderTexture, destination);
+    private void OnGUI()
+    {
+        if(objects == null)
+        {
+            if(GUI.Button(new Rect(0, 0, 100, 50), "Create"))
+            {
+                CreateCubes();
+            }
+        }
+        else
+        {
+            if(GUI.Button(new Rect(100, 0, 100, 50), "Random GPU"))
+            {
+                OnRandomizeGPU();
+            }
+        }
     }
 }
